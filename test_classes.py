5729ab290133e65dde1abbcace38c02fcaf20aaf
denis-dovgodko <dendov555@gmail.com>
2025-03-23 17:32:39 +0200

    passed

--- test_classes.py
+++ test_classes.py
@@ -4,6 +4,9 @@ class Test(metaclass=CacheMeta):
     def compute(self, x, y):
         print(f"x: {x}, y: {y}. Calculating x+y ...")
         return x + y
+    def compute2(self, x, y):
+        print(f"x: {x}, y: {y}. Calculating x^y ...")
+        return x ^ y
     
 class Test2(metaclass=CacheMeta):
     def compute(self, x, y):
